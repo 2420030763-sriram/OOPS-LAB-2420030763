09d473931f85357f58944e20ed8d2efd06247479
2420030763-sriram <2420030763@klh.edu.in>
2025-07-26 11:41:37 +0530

    Add files via upload

---
+++ all_operators.java
@@ -0,0 +1,16 @@
+package basic;
+
+public class all_operators {
+
+	public static void main(String[] args) {
+		int a = 100;
+		int b = 670;
+		int c = a+b;
+		int d = a-b;
+		int e = a*b;
+		System.out.println(c);
+		System.out.println(d);	
+		System.out.println(e);
+	}
+
+}
