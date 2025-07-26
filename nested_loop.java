2bc64d595806be8a18444c3b7defc3ea1286c346
2420030763-sriram <2420030763@klh.edu.in>
2025-07-26 15:10:26 +0530

    Add files via upload

---
+++ nested_loop.java
@@ -0,0 +1,13 @@
+package basic;
+
+public class nested_loop {
+
+	public static void main(String[] args) {
+		for(int i=1;i<10;i++) {
+			for(int j=i;j<=6;j++) {
+				System.out.println("*");
+			}
+			System.out.println();
+		}
+	}
+}
