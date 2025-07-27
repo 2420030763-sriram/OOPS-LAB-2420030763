0fbba567e8ce28f1a690dae2d422d8e07bcd0fd0
2420030763-sriram <2420030763@klh.edu.in>
2025-07-28 05:16:35 +0530

    Add files via upload

---
+++ if_else.java
@@ -0,0 +1,13 @@
+package basic;
+
+public class if_else {
+    public static void main(String[] args) {
+        int num = 4;
+
+        if (num % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+    }
+}
