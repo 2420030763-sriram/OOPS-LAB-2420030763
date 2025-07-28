82e7c89492886d17e76b91d4cdb510fd2d9f4634
2420030763-sriram <2420030763@klh.edu.in>
2025-07-29 05:29:28 +0530

    Add files via upload

---
+++ index_number.java
@@ -0,0 +1,11 @@
+package basic;
+
+public class index_number {
+
+	public static void main(String[] args) {
+		int num[]= {20,80,90,50,89,10};
+		for(int i=0;i<=5;i++) {
+			System.out.println(num[i]);
+		}
+	}
+}
